0c0907b2cfb8785a6aa57389a9d057f823b56dfa
Sergei <faoxis@mail.ru>
2016-07-11 16:06:59 +0300

    lesson 2

--- new_task.py
+++ new_task.py
@@ -2,18 +2,24 @@
 import pika
 import sys
 
+# connect to rabbitmq
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+    host='localhost'))
+# get channel - object
 channel = connection.channel()
 
+# checking existing of queue
 channel.queue_declare(queue='task_queue', durable=True)
 
+# getting a new message
 message = ' '.join(sys.argv[1:]) or "Hello World!"
+
 channel.basic_publish(exchange='',
                       routing_key='task_queue',
                       body=message,
                       properties=pika.BasicProperties(
-                         delivery_mode = 2, # make message persistent
+                          delivery_mode=2,  # make message persistent
                       ))
+
 print " [x] Sent %r" % (message,)
 connection.close()
--- worker.py
+++ worker.py
@@ -16,8 +16,11 @@ def callback(ch, method, properties, body):
     print " [x] Done"
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
+# Этот метод позволяет передавать сообщение только тем подписчикам, что освободились
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback,
-                      queue='task_queue')
 
+# Этот метод позволяет готоворит о том, что функция callback получает сообщение из очереди с именем task_queue
+channel.basic_consume(callback, queue='task_queue')
+
+# Этот метод запускает бесконечное ожидание сообщения и вызывает callback функцию когда это необходимо
 channel.start_consuming()
