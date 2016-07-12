4d80b7bb808c2964a9778906aaf60e9c9f492184
faoxis <faoxis@mail.ru>
2016-07-12 17:20:21 +0300

    now with comments

--- lesson1/receive.py
+++ lesson1/receive.py
@@ -1,15 +1,34 @@
+# -*- coding: utf-8 -*-
 import pika
 
+# Функция, которая бдет вызвана после получения сообщения из очереди
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
 
+
+
+# Подключение к брокеру на локальном хосте
 connection = pika.BlockingConnection(pika.ConnectionParameters(
                'localhost'))
 channel = connection.channel()
+
+
+
+# Проверка очереди на наличие
 channel.queue_declare(queue='hello')
 
+
+# Данный метод говорит о том, что при принятии сообщения из очереди с именем hello будет вызвана функция callback
+# no_ack=True - отключение подтверждения обработанного сообщения, по умолчанию no_ack=False
 channel.basic_consume(callback,
                       queue='hello',
                       no_ack=True)
+
+
+# Запуск бесконечного ожидания сообщений из очереди
 print ' [*] Waiting for messages. To exit press CTRL+C'
 channel.start_consuming()
+
+
+# Для просмотра существующи очередей используется команда
+# $ sudo rabbitmqctl list_queues
--- lesson1/send.py
+++ lesson1/send.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import pika
 
+# Подключение к брокеру на локальном хосте
 connection = pika.BlockingConnection(pika.ConnectionParameters(
                'localhost'))
 channel = connection.channel()
 
+# Создание очереди с именем hello, если ее не было
 channel.queue_declare(queue='hello')
 
+# Отправка сообщение с точкой обмена (для определения куда отправлять) (exchange) по умолчанию,
+# в очередь с именем hello (routing_key)
+# и с сообщением "Hello World!'
 channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hello World!')
 print " [x] Sent 'Hello World!'"
 
 connection.close()
+
+# Для просмотра существующи очередей используется команда
+# $ sudo rabbitmqctl list_queues
